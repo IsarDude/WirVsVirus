dc402d61cacbffc988faa659cea6d264657d0655
Johannes300 <johannes.volk.jv@gmail.com>
2020-03-21 18:48:58 +0100

    test

--- app/src/main/java/com/wirVsVirus/shopping/Client.java
+++ app/src/main/java/com/wirVsVirus/shopping/Client.java
@@ -6,7 +6,7 @@ import java.io.ObjectInputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.List;
- 
+
 public class Client {
     public static String IP = "127.0.0.1";
     public static int PORT = 1234;
